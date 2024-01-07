ded2cd022d08672cce7ef0bcc7fef19a531c67bc
yunuSdet <yunuskulcu84@gmail.com>
2024-01-07 04:57:00 +0300

    First task is completed

--- Task1.sql
+++ Task1.sql
@@ -12,4 +12,12 @@ WHERE length>60 and length<75;
 SELECT * FROM film
 WHERE rental_rate=0.99 AND (replacement_cost=12.99 OR replacement_cost=28.99);
 
+--Forth query
+SELECT last_name FROM customer
+WHERE first_name='Mary';
+
+
+--Fifth query
+SELECT * FROM film
+WHERE  (not length>50) AND (rental_rate!=2.99 OR rental_rate=4.99);
 
