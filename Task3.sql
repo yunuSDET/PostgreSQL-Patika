2a4024fe5753ae2aac71512c9127b91ce1952424
yunuSdet <yunuskulcu84@gmail.com>
2024-01-08 00:08:10 +0300

    Task3 is created

---
+++ Task3.sql
@@ -0,0 +1,20 @@
+
+
+--First query
+SELECT country FROM country
+WHERE country LIKE 'A%a';
+
+
+--Second query
+SELECT country FROM country
+WHERE country LIKE '_____%n';
+
+
+--Third query
+SELECT title FROM film
+WHERE title ILIKE '%t%t%t%t%';
+
+
+--Forth query
+SELECT * FROM film
+WHERE (title LIKE 'C%') AND (length>90) AND (rental_rate=2.99);
